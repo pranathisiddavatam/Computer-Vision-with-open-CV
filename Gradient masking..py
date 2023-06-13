5ac7cbfb5018b2340c9153994f875c5bd999c327
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:42:46 +0530

    Update Gradient masking..py

--- Gradient masking..py
+++ Gradient masking..py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-image = cv2.imread("C:/Users/Dama Prasoona/OneDrive/Pictures/255.png")
+image = cv2.imread("C:/Users/Pranathi/OneDrive/Pictures/255.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gradient_x = cv2.Sobel(gray, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=3)
 gradient_y = cv2.Sobel(gray, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=3)
