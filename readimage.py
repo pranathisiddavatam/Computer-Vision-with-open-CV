d58074c6dfdc95f1d7e673b0be702463fa7a17ec
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:48:54 +0530

    Update readimage.py

--- readimage.py
+++ readimage.py
@@ -5,7 +5,7 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
 
-path = "C:/Users/Dama Prasoona/Downloads/harrypotter.png"
+path = "C:/Users/Pranathi/Downloads/harrypotter.png"
 img =  cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
