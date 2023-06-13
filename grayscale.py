64eb095d550320960ba3206c1e5d4989294d9602
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:47:08 +0530

    Update grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -5,7 +5,7 @@ import numpy as np
 kernel = np.ones((2,5),np.uint8)
 print(kernel)
 
-path = "C:/Users/Dama Prasoona/Downloads/harrypotter.png"
+path = "C:/Users/Pranathi/Downloads/harrypotter.png"
 img =  cv2.imread(path)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 cv2.imshow("Lena",img)
