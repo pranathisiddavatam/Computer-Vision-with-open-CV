e36d66c2a09ec7acf9f7cab8b06b27d0ae662e42
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:46:54 +0530

    Update erosion.py

--- erosion.py
+++ erosion.py
@@ -5,7 +5,7 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
 
-path = "C:/Users/Dama Prasoona/Downloads/harrypotter.png"
+path = "C:/Users/Pranathi/Downloads/harrypotter.png"
 img =  cv2.imread(path)
 imgEroded = cv2.erode(imgDilation,kernel,iterations=2)
 cv2.imshow("Img Erosion",imgEroded)
