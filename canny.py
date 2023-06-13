4356e0ae5a2e579d1d723c2c4876eddf1167a02e
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:45:21 +0530

    Update canny.py

--- canny.py
+++ canny.py
@@ -5,7 +5,7 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
 
-path = "C:/Users/Dama Prasoona/Downloads/harrypotter.png"
+path = "C:/Users/Pranathi/Downloads/harrypotter.png"
 img =  cv2.imread(path)
 imgCanny = cv2.Canny(img,100,200)
 cv2.imshow("Img Canny",imgCanny)
