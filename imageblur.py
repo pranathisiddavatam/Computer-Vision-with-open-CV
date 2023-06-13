461f6c4c310c70bc3295ab373f80fc3ed0c4498b
pranathisiddavatam <113352841+pranathisiddavatam@users.noreply.github.com>
2023-06-13 20:47:45 +0530

    Update imageblur.py

--- imageblur.py
+++ imageblur.py
@@ -5,7 +5,7 @@ import numpy as np
 kernel = np.ones((5,5),np.uint8)
 print(kernel)
 
-path ="C:/Users/Dama Prasoona/Downloads/harrypotter.png"
+path ="C:/Users/Pranathi/Downloads/harrypotter.png"
 img =  cv2.imread(path)
 
 imgBlur = cv2.GaussianBlur(img,(7,7),0)
